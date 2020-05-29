f953062c55e7518936440cb931c4bd077c1cbe10
Ghevi <ghevi96@gmail.com>
2020-05-30 01:09:53 +0200

    modified:   src/app/app.module.ts 	modified:   src/app/recipes/recipe-detail/recipe-detail.component.html 	modified:   src/app/recipes/recipe-detail/recipe-detail.component.ts 	modified:   src/app/recipes/recipe-edit/recipe-edit.component.css 	modified:   src/app/recipes/recipe-edit/recipe-edit.component.html 	modified:   src/app/recipes/recipe-edit/recipe-edit.component.ts 	modified:   src/app/recipes/recipe-list/recipe-list.component.ts 	modified:   src/app/recipes/recipe.service.ts 	modified:   src/app/recipes/recipes.component.ts 	modified:   src/app/shopping-list/shopping-edit/shopping-edit.component.html 	modified:   src/app/shopping-list/shopping-edit/shopping-edit.component.ts 	modified:   src/app/shopping-list/shopping-list.service.ts

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,11 +4,13 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
   // recipeSelected = new EventEmitter<Recipe>();
   // recipeSelected = new Subject<Recipe>();
+  recipeChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -43,5 +45,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipeChanged.next(this.recipes.slice());
   }
 }
