e685ed5e58c0b0e26129b0a2234d97062e1ba79e
Ghevi <ghevi96@gmail.com>
2020-05-31 22:51:35 +0200

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts 	modified:   src/app/header/header.component.html 	modified:   src/app/header/header.component.ts 	modified:   src/app/recipes/recipe.service.ts 	new file:   src/app/recipes/recipes-resolver.service.ts 	new file:   src/app/shared/data-storage.service.ts

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map, tap } from "rxjs/operators";
+
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        "https://ng-course-recipe-book-4e1e1.firebaseio.com/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        "https://ng-course-recipe-book-4e1e1.firebaseio.com/recipes.json"
+      )
+      .pipe(
+        map((recipes) => {
+          // map is an rxjs operator
+          return recipes.map((recipe) => {
+            // map is just a function called on the recipes array
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+  }
+}
