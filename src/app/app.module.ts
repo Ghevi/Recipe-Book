eca987058d81cb8c8317f9b18d4fd0d246960fd7
Ghevi <ghevi96@gmail.com>
2020-06-03 00:36:35 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.module.ts 	modified:   src/app/shopping-list/shopping-edit/shopping-edit.component.ts 	modified:   src/app/shopping-list/shopping-list.component.html 	modified:   src/app/shopping-list/shopping-list.component.ts 	new file:   src/app/shopping-list/store/shopping-list.actions.ts 	new file:   src/app/shopping-list/store/shopping-list.reducer.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -9,24 +10,23 @@ import { CoreModule } from './core.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-import { LoggingService } from './logging.service';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-    // DropdownDirective, Components must be only declared once in the app
-    // LoadingSpinnerComponent,
-    // AlertComponent,
-    // PlaceholderDirective
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    SharedModule, // We import this to get the 4 commented components above
-    CoreModule
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
+    SharedModule, // We import this to get the 4 commented components below (they go into declarations)
+    CoreModule,
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// DropdownDirective, Components must be only declared once in the app
+// LoadingSpinnerComponent,
+// AlertComponent,
+// PlaceholderDirective
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
 
 @Component({
@@ -11,20 +12,22 @@ import { LoggingService } from '../logging.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ingredients: Ingredient[]}>;
   private subscription: Subscription;
 
   constructor(private slService: ShoppingListService,
-              private logginService: LoggingService) { }
+              private logginService: LoggingService,
+              private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged
-      .subscribe(
-        (ingredients: Ingredient[]) => {
-          this.ingredients = ingredients;
-        }
-      );
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.slService.getIngredients();
+    // this.subscription = this.slService.ingredientsChanged
+    //   .subscribe(
+    //     (ingredients: Ingredient[]) => {
+    //       this.ingredients = ingredients;
+    //     }
+    //   );
 
     this.logginService.printLog('Hello from ShoppingListComponent ngOnInit');
   }
@@ -34,7 +37,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    // this.subscription.unsubscribe();
   }
 }
 
