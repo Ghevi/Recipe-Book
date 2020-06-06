1e3f06f120c4eabf8ac7c9ab7a3413ff962cd6fa
Ghevi <ghevi96@gmail.com>
2020-06-07 01:03:05 +0200

    modified:   src/app/app.module.ts 	modified:   src/app/auth/auth.component.ts 	modified:   src/app/auth/auth.service.ts 	modified:   src/app/auth/store/auth.actions.ts 	modified:   src/app/auth/store/auth.effects.ts 	modified:   src/app/auth/store/auth.reducer.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -103,7 +103,7 @@ export class AuthService {
     if (loadedUser.token) {
       // this.user.next(loadedUser);
       this.store.dispatch(
-        new AuthActions.Login({
+        new AuthActions.AuthenticateSuccess({
           email: loadedUser.email,
           userId: loadedUser.id,
           token: loadedUser.token,
@@ -145,7 +145,7 @@ export class AuthService {
     const user = new User(email, userId, token, expirationDate);
     // this.user.next(user);
     this.store.dispatch(
-      new AuthActions.Login({
+      new AuthActions.AuthenticateSuccess({
         email: email,
         userId: userId,
         token: token,
--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,12 +1,13 @@
 import { Action } from "@ngrx/store";
 
-export const LOGIN_START = '[Auth] Login Start';
-export const LOGIN = "[Auth] Login";
+export const LOGIN_START = "[Auth] Login Start";
+export const AUTHENTICATE_SUCCESS = "[Auth] Authenticate Success";
+export const AUTHENTICATE_FAIL = "[Auth] Authenticate Fail";
+export const SIGNUP_START = "[Auth] Signup Start";
 export const LOGOUT = "[Auth] Logout";
 
-export class Login implements Action {
-  readonly type = LOGIN;
-  
+export class AuthenticateSuccess implements Action {
+  readonly type = AUTHENTICATE_SUCCESS;
 
   constructor(
     public payolad: {
@@ -17,9 +18,30 @@ export class Login implements Action {
     }
   ) {}
 }
+export class LoginStart implements Action {
+  readonly type = LOGIN_START;
+
+  constructor(public payload: { email: string; password: string }) {}
+}
+export class AuthenticateFail implements Action {
+  readonly type = AUTHENTICATE_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export class SignupStart implements Action {
+  readonly type = SIGNUP_START;
+
+  constructor(public payload: { email: string; password: string }) {}
+}
 
 export class Logout implements Action {
   readonly type = LOGOUT;
 }
 
-export type AuthActions = Login | Logout;
+export type AuthActions =
+  | AuthenticateSuccess
+  | Logout
+  | LoginStart
+  | AuthenticateFail
+  | SignupStart;
